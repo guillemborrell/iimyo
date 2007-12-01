c4c714c81470ce9fcc5e06ed9d084cf8b40427fa
guillem <guillem@b86b3439-6f2e-4151-b5ad-4439e4173a73>
2007-12-01 12:26:23 +0000

    Despues del merge este directorio ya no tiene sentido
    git-svn-id: https://forja.rediris.es/svn/iimyo/trunk@15 b86b3439-6f2e-4151-b5ad-4439e4173a73

--- material_extra/ejercicio_espectral_scripts/R.m
+++
@@ -1,9 +0,0 @@
-function ret = R(M)
-
-% Dificil de descifrar.  Comentario pendiente
-% Fuciona.
-
-dxxb = dxx(M+2)*b(M);% Primer termino
-x6b = xn(M+2,6)*b(M);% Segundo termino
-x2_3b = 3*xn(M+2,2)*b(M);% Tercer termino
-ret = join(dxxb,x6b) - x6b - join(x2_3b,x6b);
--- material_extra/ejercicio_espectral_scripts/b.m
+++
@@ -1,7 +0,0 @@
-function ret=b(M)
-
-%  argumentos:
-%  M :: escalar entero
-
-x2 = xn(M,2);
-ret = join(eye(M),x2)-x2;
