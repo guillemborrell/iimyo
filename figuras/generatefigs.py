8cae7a3abba5a9633f999d4c620b5d4245910d80
guillem <guillem@b86b3439-6f2e-4151-b5ad-4439e4173a73>
2008-01-16 16:30:39 +0000

    he modificado la aplicacion que genera las figuras, ahora tambien genera eps para ser procesados por hevea y generar automaticamente el html del libro
    git-svn-id: https://forja.rediris.es/svn/iimyo/trunk@31 b86b3439-6f2e-4151-b5ad-4439e4173a73

--- figuras/generatefigs.py
+++ figuras/generatefigs.py
@@ -1,4 +1,26 @@
-from os import system,chdir
+#!/usr/bin/python
+
+from os import system,chdir,listdir
+from sys import argv
+
+help = False
+if '-h' in argv or '--help' in argv:
+    help = True
+    print """
+    This application is used to generate all the figures for the book
+    IIMyO.
+
+    Ran without arguments generates the figures in pdf. You can use the
+    following options:
+
+    * --help, -h: Displays this help
+
+    * --eps, -e: Figures in eps. Needed to build the book in html.
+    """
+    
+eps=False
+if '-e' in argv or '--eps' in argv: eps=True
+
 
 dia=['Diagram1','Diagram2','Diagram3','Diagram4','Diagram6',
      'Diagram7','Diagram8','Diagram9','Diagram10','Diagram12',
@@ -10,49 +32,71 @@ octave=['figuraejemplo1','figuraejemplo2','calor','calorevol',
 python=['figuraejemplo3','figuraejemplo4','figuraejemplo5',
         'figuraejemplo6','figuraejemplo7','fzero']
 
-for diagram in dia:
-    try:
-        system('dia --export=%s.eps --filter=eps %s.dia'%(diagram,diagram))
-        system('epstopdf %s.eps'%(diagram))
-        system('rm %s.eps'%(diagram))
-        print('*****************')
-        print('diagram generated for %s'%(diagram))
-        print('*****************')
-    except:
-        print('diagram %s failed, check that dia is installed'%(diagram))
+if eps and not help:
+    for filename in listdir('.'):
+        if '.pdf' in filename:
+            filename=filename[:-4]
+            try:
+                print filename
+                system('pdftops %s.pdf'%(filename))
+                system('ps2epsi %s.ps'%(filename))
+                system('rm %s.ps'%(filename))
+                system('mv %s.epsi %s.eps'%(filename,filename))
+                print 'figure %s converted to eps'%(filename)
+            except:
+                print 'figure %s not converted'%(filename)
 
-for script in octave:
-    try:
-        system('octave -q %s.m'%(script))
-        system('epstopdf %s.eps'%(script))
-        system('rm %s.eps'%(script))
-        print('*****************')
-        print('figure generated for %s'%(script))
-        print('*****************')
-    except:
-        print('figure %s failed, check that octave is installed'%(script))
 
-for script in python:
-    try:
-        system('python %s.py'%(script))
-        print('*****************')
-        print('figure generated for %s'%(script))
-        print('*****************')
-    except:
-        print('figure %s failed, check that matplotlib is installed'%(script))
+if not help:
+    for diagram in dia:
+        try:
+            system('dia --export=%s.eps --filter=eps %s.dia'%(diagram,diagram))
+            if not eps:
+                system('epstopdf %s.eps'%(diagram))
+                system('rm %s.eps'%(diagram))
+            print('*****************')
+            print('diagram generated for %s'%(diagram))
+            print('*****************')
+        except:
+            print('diagram %s failed, check that dia is installed'%(diagram))
 
+if not help:
+    for script in octave:
+        try:
+            system('octave -q %s.m'%(script))
+            if not eps:
+                system('epstopdf %s.eps'%(script))
+                system('rm %s.eps'%(script))
+            print('*****************')
+            print('figure generated for %s'%(script))
+            print('*****************')
+        except:
+            print('figure %s failed, check that octave is installed'%(script))
+    
+    for script in python:
+        try:
+            system('python %s.py'%(script))
+            print('*****************')
+            print('figure generated for %s'%(script))
+            print('*****************')
+        except:
+            print('figure %s failed, check that matplotlib is installed'%(script))
+                    
 # tratamiento especial para ejercicio_espectral
 # cuidado con el cambio de directorio, esta parte
 # debe estar siempre al final
 
 chdir('ejercicio_espectral_scripts')
-try:
-    system('octave -q main.m')
-    system('epstopdf solucion.eps')
-    system('epstopdf error.eps')
-    system('epstopdf convergencia.eps')
-    print('************')
-    print('figuras para ejercicio espectral generadas')
-    print('************')
-except:
-    print('figures for ejercicio espectral failed')
+if not help:
+    try:
+        system('octave -q main.m')
+        if not eps:
+            system('epstopdf solucion.eps')
+            system('epstopdf error.eps')
+            system('epstopdf convergencia.eps')
+        print('************')
+        print('figuras para ejercicio espectral generadas')
+        print('************')
+    except:
+        print('figures for ejercicio espectral failed')
+
