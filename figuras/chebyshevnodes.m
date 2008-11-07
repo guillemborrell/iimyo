920e22f2855088c851059c066500e85ef93dec4c
guillem <guillem@b86b3439-6f2e-4151-b5ad-4439e4173a73>
2008-11-07 10:37:17 +0000

    Mejores figuras en gnuplot.  No se veían los nodos
    git-svn-id: https://forja.rediris.es/svn/iimyo/trunk@80 b86b3439-6f2e-4151-b5ad-4439e4173a73

--- figuras/chebyshevnodes.m
+++ figuras/chebyshevnodes.m
@@ -5,8 +5,8 @@ y=fit(x);
 ycont=fit(xcont);
 c=vander(x)\y';
 yfit=polyval(c',xcont); 
-p=plot(x,y,'r.',xcont,ycont,'k',xcont,yfit,'b');
-set(p(1),'markersize',2)
+p=plot(x,y,'r*',xcont,ycont,'k',xcont,yfit,'b');
+set(p(1),'markersize',10)
 set(p(2),'linewidth',2)
 set(p(3),'linewidth',3)
 axis([-1,1,-0.1,1.1]);
