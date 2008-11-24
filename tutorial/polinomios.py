d6f4e7113b7e7ed3940a0ff8641f5b743de296df
guillem <guillem@b86b3439-6f2e-4151-b5ad-4439e4173a73>
2008-11-24 20:41:47 +0000

    Terminado el ejercicio del polinomio para Patricia
    git-svn-id: https://forja.rediris.es/svn/iimyo/trunk@85 b86b3439-6f2e-4151-b5ad-4439e4173a73

---
+++ tutorial/polinomios.py
@@ -0,0 +1,36 @@
+import numpy as N
+import scipy as S
+import pylab as P
+from scipy import interpolate
+
+k = lambda M: M*(1+1/(1+M**2))**0.23
+
+Mv = S.linspace(0,1,100)
+kv = k(Mv)
+
+P.plot(Mv,kv)
+P.xlabel('M')
+P.ylabel('k')
+
+
+c = S.polyfit(kv,Mv,2)
+
+kvv = S.linspace(0,1.1,100)
+Mvv = S.polyval(c,kvv)
+
+P.figure(2)
+P.plot(Mv,kv,'b',Mvv,kvv,'g')
+P.xlabel('M')
+P.ylabel('k')
+
+
+exact = interpolate.interp1d(Mv,kv)
+appro = interpolate.interp1d(Mvv,kvv)
+rg = S.linspace(0.1,1,100)
+
+P.figure(3)
+P.plot(Mv,exact(rg)-appro(rg))
+P.xlabel('M')
+P.ylabel('Error')
+P.show()
+
