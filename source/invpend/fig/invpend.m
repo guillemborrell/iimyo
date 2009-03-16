61b55dc181601ca5d9eb79134bf7cfe0608c9cc9
Guillem Borrell i Nogueras <guillem@peret.dmt.upm.es>
2009-03-16 11:46:50 +0100

    He mejorado significativamente el script del pendulo, pero ahora toca hacer un merge complicado

--- source/invpend/fig/invpend.m
+++ source/invpend/fig/invpend.m
@@ -3,15 +3,19 @@ clc
 
 %% Solucion estable
 
-k = 10; % Constante de prueba
-l = 0.1; % Longitud adimensional
-
 tau = linspace(0,4*pi,100); % Tiempo adimensional
 
-%%% Ecuacion diferencial linealizada del pendulo invertido
-pend = @(x,t) [x(2),x(1)*(sin(t)-k)/l];
+%%% Generador de la ecuacion:
+%%% Parametros: k,l
+%%% Variables: x,t
 
-theta = lsode(pend,[0.01 0],tau); %Integracion
+genpend = @(k,l) @(x,t) [x(2),x(1)*(sin(t)-k)/l];
+
+%%%
+
+				% k = 10
+				% l = 0.1
+theta = lsode(genpend(10,0.1),[0.01 0],tau); %Integracion
 
 figure(1);
 clf;
@@ -29,15 +33,10 @@ ylabel('y/a')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% k para que la solucion sea inestable
-
-k = 0.1;
-
-%%% Ecuacion diferencial linealizada del pendulo invertido
-pend = @(x,t) [x(2),x(1)*(sin(t)-k)/l];
-
-theta = lsode(pend,[0.01 0],tau); %Integracion
-
+%% Caso inestable
+				% k = 0.1
+				% l = 0.1
+theta = lsode(genpend(0.1,0.1),[0.01 0],tau); %Integracion
 
 figure(2); 
 clf;
@@ -62,40 +61,16 @@ clf;
 tau = linspace(0,10*pi,100); % Mas tiempo adimensional para comprobar la
 				% convergencia de la solucion
 
-k = 0.9;
+idx = 1;
+klist = [0.9,1.1,1.2];
 
-%%% Ecuacion diferencial linealizada del pendulo invertido
-pend = @(x,t) [x(2),x(1)*(sin(t)-k)/l];
+for k = klist
+  theta = lsode(genpend(k,0.1),[0.01 0],tau); %Integracion
 
-theta = lsode(pend,[0.01 0],tau); %Integracion
-
-subplot(3,1,1);
-plot(tau,theta(:,1));
-xlabel('\tau')
-ylabel('\theta')
-title('k=0.9')
-
-k = 1.1;
-
-%%% Ecuacion diferencial linealizada del pendulo invertido
-pend = @(x,t) [x(2),x(1)*(sin(t)-k)/l];
-
-theta = lsode(pend,[0.01 0],tau); %Integracion
-
-subplot(3,1,2);
-plot(tau,theta(:,1))
-xlabel('\tau')
-ylabel('\theta')
-title('k=1.1')
-
-k = 1.2;
-%%% Ecuacion diferencial linealizada del pendulo invertido
-pend = @(x,t) [x(2),x(1)*(sin(t)-k)/l];
-
-theta = lsode(pend,[0.01 0],tau); %Integracion
-
-subplot(3,1,3);
-plot(tau,theta(:,1))
-xlabel('\tau')
-ylabel('\theta')
-title('k=1.2')
+  subplot(3,1,idx);
+  plot(tau,theta(:,1));
+  xlabel('\tau');
+  ylabel('\theta');
+  title(['k=',+num2str(k)]);
+  idx++;
+end
