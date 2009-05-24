fcca1c7a3a0d94b2cb2120f65ee369bc687d942f
Guillem Borrell i Nogueras <guillem@desktop.borrell>
2009-05-24 14:06:26 +0200

    Ahora el ejemplo en java corre

--- source/ref/testarray.java
+++ source/ref/testarray.java
@@ -1,24 +1,28 @@
 class TestArray {
-    public static void main{String args[]} {
+    public static void main(String args[]) {
 	int a[][] = new int[3][3];
 	int i,j,k = 0;
 
-	for(i=0,i<3,i++){
-	    for(j=0,j<3,j++){
+	for(i=0;i<3;i++){
+	    for(j=0;j<3;j++){
 		a[i][j] = k;
 		k++;
 	    }
 	}
 	System.out.print("Indexo escalares");
-	for(i=0,i<3,i++){
-	    for(j=0,j<3,j++){
+	System.out.println();
+	for(i=0;i<3;i++){
+	    for(j=0;j<3;j++){
 		System.out.print(a[i][j]);
 	    }
+	    System.out.println();
 	}
 	System.out.println();
 	System.out.print("Indexo filas");
-	for(i=0,i<3,i++){
+	System.out.println();
+	for(i=0;i<3;i++){
 	    System.out.print(a[i]);
 	}
+	System.out.println();
     }
 }
