90a45dd2375366e70703aca97d40acc692bebfd0
Guillem Borrell i Nogueras <guillem@peret.dmt.upm.es>
2009-06-26 22:39:38 +0200

    Mejorando el tema del burgers

--- source/burgers/burgersnd.m
+++ source/burgers/burgersnd.m
@@ -1,10 +1,11 @@
 clear
+clf
 
 %%% Linear operator L(uhat,kx)
 L = @(uhat,kx) -(kx.^2).*uhat;
 
 %% setting up
-N=128;
+N=64;
 u0 = @(x) 10.*sin(x);
 x = linspace(-pi,pi,N+1);
 x = x(1:N);
@@ -13,32 +14,20 @@ uhat0 = fft(u0(x));
 
 %%% Setting wavenumber as a parameter
 Lh = @(uhat) L(uhat,kx);
-Dh = @(uhat) D(uhat,kx,N);
+Dh = @(uhat) Dnd(uhat,kx,N); % change here
 
 %%% Time integration setup
-CFL = 0.5;
+CFL = 0.9;
 h = CFL*2/(max(kx).^2+max(u0(x))) %step
 tend = 0.1;
 
 %%% Explicit Euler u(n+1) = u(n) + h(L(u(n))-D(u(n)))
+hold on
+
 t = 0;
 while (t < tend)
-  uhat0 = uhat0 + h*(0.01*Lh(uhat0)-Dh(uhat0));
+  uhat0 = uhat0 + h*(Lh(uhat0)-Dh(uhat0));
   t = t + h;
 end
 
-%%% analysis of the aliasing thing
-
-figure(1)
-clf
-plot(abs(uhat0));
-title('Power spectra of u at tend');
-ylabel('u');
-xlabel('kx');
-
-figure(2)
-clf
 plot(real(ifft(uhat0)));
-title('u at tend');
-ylabel('u');
-xlabel('x');
