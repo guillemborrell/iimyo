c900fe4a7e5a065373361934a91069a5a70c8da9
Guillem Borrell <guillemborrell@gmail.com>
2016-08-13 13:49:45 +0200

    Cambios para poder subir la documentación como un proyecto de Appengine.

--- source/conf.py
+++ source/conf.py
@@ -36,7 +36,7 @@ master_doc = 'contenidos'
 
 # General information about the project.
 project = u'iimyo2'
-copyright = ur'2005-2011, Guillem Borrell i Nogueras'
+copyright = ur'2005-2016, Guillem Borrell i Nogueras'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
